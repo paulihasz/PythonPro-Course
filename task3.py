72b8ebdd35562cc8ea613e8744bea362bd624e3b
paulihasz <pauli.hasz89@gmail.com>
2026-04-15 09:33:53 +0200

    Delete task3.py

--- task3.py
+++
@@ -1,8 +0,0 @@
-# analiza stringów
-
-text = " Python is awesome! "
-print(text.strip())  # Python is awesome !
-print(text.upper())  # PYTHON IS AWESOME!
-print(text.lower())  # python is awesome!
-print(text.replace("awesome", "great"))  # Python is great!
-print(text[4])
